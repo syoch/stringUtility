948eba3a1c6ef648b8dd4742209d7748b57cc31a
syoch <hacksyoch@gmail.com>
2021-02-11 09:22:24 +0900

    update: add isDec/Hex

--- strutil.h
+++ strutil.h
@@ -6,6 +6,18 @@
 
 namespace util
 {
+    template <typename T>
+    bool isDec(T val)
+    {
+        return inRange<T>('0', val, '9');
+    }
+
+    template <typename T>
+    bool isHex(T val)
+    {
+        return isDec(val) || inRange<T>('a', val, 'f');
+    }
+
     int _toInt(std::wstring _text);
     std::string wstr2str(std::wstring str);
     std::wstring str2wstr(std::string str);
