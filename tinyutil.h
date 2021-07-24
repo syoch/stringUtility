8854c1c5573b10b9b3e97cc50075d0ff8508ac50
syoch <hacksyoch@gmail.com>
2021-07-24 17:04:12 +0900

    update: change header name

---
+++ tinyutil.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef SYOCH_STRUTIL_H
+#define SYOCH_STRUTIL_H
+
+#include <string>
+
+namespace util {
+template <typename T>
+bool inRange(T min, T val, T max) {
+  return min <= val && val <= max;
+}
+
+template <typename T>
+bool isDec(T val) {
+  return inRange<T>('0', val, '9');
+}
+
+template <typename T>
+bool isHex(T val) {
+  return isDec(val) || inRange<T>('a', val, 'f');
+}
+
+int _toInt(std::wstring _text);
+std::string wstr2str(std::wstring str);
+std::wstring str2wstr(std::string str);
+}  // namespace util
+
+#endif
